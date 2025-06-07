e646f7d9fc333ec209d8b182ebe400854d22d5fb
Daniel Bravo <dani.bravos@duocuc.cl>
2025-06-07 01:49:09 -0400

    Actualizacion de codigo Eco-Market

--- microservice-EcoMarket/src/main/resources/import.sql
+++ microservice-EcoMarket/src/main/resources/import.sql
@@ -1,5 +1,6 @@
-INSERT INTO courses(name,teacher) VALUES('Matematicas','Profesor A');
-INSERT INTO courses(name,teacher) VALUES('Historia','Profesor B');
-INSERT INTO courses(name,teacher) VALUES('Ciencias','Profesor C');
-INSERT INTO courses(name,teacher) VALUES('Literatura','Profesor D');
-INSERT INTO courses(name,teacher) VALUES('Educación Fisica','Profesor E');
+INSERT INTO ecomarkets (nombre, direccion, ciudad, region, pais, jefe_nombre, jefe_correo, jefe_telefono) VALUES
+('EcoMercado Central', 'Av. Libertad 1234', 'Santiago', 'Región Metropolitana', 'Chile', 'María González', 'maria.gonzalez@ecomarket.cl', '+56911112222'),
+('EcoSur', 'Calle 8 Oriente 456', 'Concepción', 'Biobío', 'Chile', 'Luis Torres', 'luis.torres@ecomarket.cl', '+56933334444'),
+('EcoNorte', 'Ruta 5 Km 12', 'La Serena', 'Coquimbo', 'Chile', 'Andrea Pérez', 'andrea.perez@ecomarket.cl', '+56955556666'),
+('EcoValpo', 'Av. Argentina 789', 'Valparaíso', 'Valparaíso', 'Chile', 'Carlos Rojas', 'carlos.rojas@ecomarket.cl', '+56977778888'),
+('EcoTemuco', 'Camino al Volcán 1010', 'Temuco', 'La Araucanía', 'Chile', 'Paula Díaz', 'paula.diaz@ecomarket.cl', '+56999990000');
